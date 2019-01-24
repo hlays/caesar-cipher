3804275abf88925d0cf959c8eb5943a49c2109fb
hlays <lays.hidani@gmail.com>
2019-01-24 17:34:53 -0200

    Alterações JS

--- src/cipher.js
+++ src/cipher.js
@@ -1,2 +1,35 @@
-// window.cipher = {
-// };
+window.onload = (function () {
+  const cipherOffset = document.getElementById('cipher-offset').value;
+  const cipherText = document.getElementById('cipher-text').value;
+  const btn = document.getElementById('cipher-button');
+
+  btn.addEventListener('click', cipher, false);
+  
+  function cipher() {
+    console.log(1)
+    let arrCipher = [];
+    for (i in cipherText) {    
+      let letterOffset = ((cipherText[i].charCodeAt() - 65 + cipherOffset) % 26) + 65;
+      arrCipher.push(letterOffset);
+    }
+      
+    const arrCiphered = [];
+    for (i in arrCipher) {
+      return String.fromCharCode.apply(null, arrCipher);
+      
+      // let cipheredResult = String.fromCharCode.apply(null, arrCipher[i]);
+      // console.log(cipheredResult);
+      
+      // arrCiphered.push(cipheredResult);
+  
+    }  
+  }
+  
+})()
+
+
+
+
+// console.log('resultado', cipher(2, 'ALO'));
+
+
