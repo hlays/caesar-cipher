08ff2479f88fa10dc1683cc9c4e5e7ee665de6ed
hlays <lays.hidani@gmail.com>
2019-01-25 18:42:39 -0200

    Remoção do botão "cifrar", atualização do readme

--- src/cipher.js
+++ src/cipher.js
@@ -1,29 +1,25 @@
 window.onload = (function () {
   const cipherOffset = document.getElementById('cipher-offset').value;
   const cipherText = document.getElementById('cipher-text').value;
-  const btn = document.getElementById('cipher-button');
+  const cipherResult = document.getElementById('cipher-result');
 
-  btn.addEventListener('click', cipher, false);
   
-  function cipher() {
-    console.log(1)
+  function cipher(cipherOffset, cipherText) {
     let arrCipher = [];
     for (i in cipherText) {    
       let letterOffset = ((cipherText[i].charCodeAt() - 65 + cipherOffset) % 26) + 65;
       arrCipher.push(letterOffset);
     }
-      
+  
+    let it = String.fromCharCode.apply(null, arrCipher);
+    
     const arrCiphered = [];
     for (i in arrCipher) {
       return String.fromCharCode.apply(null, arrCipher);
-      
-      // let cipheredResult = String.fromCharCode.apply(null, arrCipher[i]);
-      // console.log(cipheredResult);
-      
-      // arrCiphered.push(cipheredResult);
-  
-    }  
+    } 
   }
+  
+  cipherResult.addEventListener('input', cipher);
   
 })()
 
