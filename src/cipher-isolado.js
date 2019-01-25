adfc10186225ca9046f03df6afbc7591dee52d04
hlays <lays.hidani@gmail.com>
2019-01-25 19:23:57 -0200

    alterações JS, ainda não funcionando

--- src/cipher-isolado.js
+++ src/cipher-isolado.js
@@ -1,14 +1,35 @@
 function cipher(cipherOffset, cipherThis) {
-  let arrCipher = [];
-  for (i in cipherThis) {    
-    let letterOffset = ((cipherThis[i].charCodeAt() - 65 + cipherOffset) % 26) + 65;
-    arrCipher.push(letterOffset);
-  }
+  if (cipherOffset < 1 || cipherThis.length < 1) {
+    alert(':/ Dados inseridos inválidos. Tente de novo...');
+  } else {
     
-  const arrCiphered = [];
-  for (i in arrCipher) {
-    return String.fromCharCode.apply(null, arrCipher);
-  }  
+    const arrCiphered = [];
+    // Se uppercase
+    for (i in cipherThis) {    
+      let arrCipher = [];
+      if (cipherThis[i] >= 65 && cipherThis[i] <= 90) {
+        let letterOffset = ((cipherThis[i].charCodeAt() - 65 + cipherOffset) % 26) + 65;
+        arrCipher.push(letterOffset);
+        for (i in arrCipher) {
+          return String.fromCharCode.apply(null, arrCipher);
+        } 
+    // se lowercase
+      } else if (cipherThis[i] >= 97 && cipherThis[i] <= 122) {
+        for (i in cipherThis) {    
+          let arrCipher = [];
+          let letterOffset = ((cipherThis[i].charCodeAt() - 97 + cipherOffset) % 26) + 97;
+          arrCipher.push(letterOffset);
+        }
+        const arrCiphered = [];
+        for (i in arrCipher) {
+          return String.fromCharCode.apply(null, arrCipher);
+        } 
+      } 
+    } 
+  }
 }
 
-console.log('=> resultado', cipher(2, 'ALO'));
+
+// teste:
+console.log('=> resultado', cipher(2, 'ALO')); // => CNQ
+console.log('=> resultado', cipher(2, 'alo')); // => CNQ
--- src/cipher.js
+++ src/cipher.js
@@ -1,31 +1,23 @@
-window.onload = (function () {
-  const cipherOffset = document.getElementById('cipher-offset').value;
-  const cipherText = document.getElementById('cipher-text').value;
-  const cipherResult = document.getElementById('cipher-result');
+let cipherOffset = document.getElementById('cipher-offset').value;
+let cipherText = document.getElementById('cipher-text').value;
+let cipherResult = document.getElementById('cipher-result');
 
-  
-  function cipher(cipherOffset, cipherText) {
-    let arrCipher = [];
-    for (i in cipherText) {    
-      let letterOffset = ((cipherText[i].charCodeAt() - 65 + cipherOffset) % 26) + 65;
-      arrCipher.push(letterOffset);
-    }
-  
-    let it = String.fromCharCode.apply(null, arrCipher);
-    
-    const arrCiphered = [];
-    for (i in arrCipher) {
-      return String.fromCharCode.apply(null, arrCipher);
-    } 
+function cipher(cipherOffset, cipherText) {
+  let arrCipher = [];
+  for (i in cipherText) {    
+    let letterOffset = ((cipherText[i].charCodeAt() - 65 + cipherOffset) % 26) + 65;
+    arrCipher.push(letterOffset);
   }
   
-  cipherResult.addEventListener('input', cipher);
+  for (i in arrCipher) {
+    cipherResult.value = String.fromCharCode.apply(null, arrCipher);
+  } 
+    
+}
+
+cipherText.addEventListener('input', cipher);
   
-})()
 
 
-
-
-// console.log('resultado', cipher(2, 'ALO'));
 
 
