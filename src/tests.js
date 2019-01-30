0110fdb83c5c059917f7b3c614b035ee7524227e
hlays <lays.hidani@gmail.com>
2019-01-30 21:33:41 +0000

    alterações da ordem dos testes no arquivo tests

--- src/tests.js
+++ src/tests.js
@@ -1,9 +1,9 @@
 function test(funcao, strOriginal, offset, resultadoEsperado) {
-  var result = funcao(offset, strOriginal);
+  let result = funcao(offset, strOriginal);
   if (result === resultadoEsperado) {
-    console.log("=> Correto! :) input:", strOriginal, "-> Output:", result, "===", resultadoEsperado, '(Resultado esperado)');
+    console.log("=> Correto! :) input:", result, "===", resultadoEsperado);
   } else {
-    console.log("=> => Errado! :/ input:", strOriginal, "-> Output:", result, "!==", resultadoEsperado, '(Resultado esperado)');
+    console.log("<= Errado! :/ input:", result, "!==", resultadoEsperado);
   }
 }
 
@@ -16,12 +16,12 @@ test(encode, 'alo', 26, 'alo');
 test(encode, 'Hello World!', 2, 'Jgnnq Yqtnf!');
 test(encode, 'Hello World!', 5, 'Mjqqt Btwqi!');
 test(encode, 'Hello World!', 250, 'Xubbe Mehbt!');
-test(encode, ' ', 2, ' ');
-test(encode, ' ', 20, ' ');
-test(encode, ' ', 2000000, ' ');
 test(encode, '123', 2, '123');
 test(encode, '123', 5, '123');
 test(encode, '123', 5500000, '123');
+test(encode, ' ', 2, ' ');
+test(encode, ' ', 20, ' ');
+test(encode, ' ', 2000000, ' ');
 
 console.log(':::::::::::Descifrar:::::::::::');
 
@@ -30,12 +30,12 @@ test(decode, 'alo', 26, 'alo');
 test(decode, 'Jgnnq Yqtnf!', 2, 'Hello World!'); 
 test(decode, 'Mjqqt Btwqi!', 5, 'Hello World!'); //Hello =orld!
 test(decode, 'Xubbe Mehbt!', 250, 'Hello World!'); //.KRRU =UXRJ!
-test(decode, ' ', 2, ' ');
-test(decode, ' ', 20, ' ');
-test(decode, ' ', 2000000, ' ');
 test(decode, '123', 2, '123');
 test(decode, '123', 5, '123');
 test(decode, '123', 5500000, '123');
+test(decode, ' ', 2, ' ');
+test(decode, ' ', 20, ' ');
+test(decode, ' ', 2000000, ' ');
 
 
 
