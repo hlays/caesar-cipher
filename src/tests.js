79411b6ee620d3b51067128522c0d5c9d435caf6
hlays <lays.hidani@gmail.com>
2019-01-31 14:44:19 -0200

    add testes: novos testes para símbolos | html: autocomplete offno input

--- src/tests.js
+++ src/tests.js
@@ -12,33 +12,35 @@ function test(funcao, strOriginal, offset, resultadoEsperado) {
 console.log('--- Cifrar >>>');
 
 
+test(encode, ' ', 2, ' ');
+test(encode, ' ', 20, ' ');
+test(encode, ' ', 2000000, ' ');
+test(encode, '123', 2, '123');
+test(encode, '123', 5, '123');
+test(encode, '123', 5500000, '123');
+test(encode, '^´=-', 5500000, '^´=-');
 test(encode, 'uvxwyz', 5, 'zacbde');
 test(encode, 'alo', 2, 'cnq');
 test(encode, 'alo', 26, 'alo');
 test(encode, 'Hello World!', 2, 'Jgnnq Yqtnf!');
 test(encode, 'abcABC fghFGH 123', 5, 'fghFGH klmKLM 123');
-test(encode, '123', 2, '123');
-test(encode, '123', 5, '123');
-test(encode, '123', 5500000, '123');
-test(encode, ' ', 2, ' ');
-test(encode, ' ', 20, ' ');
-test(encode, ' ', 2000000, ' ');
 
 
 
 console.log('--- Descifrar <<<');
 
+test(encode, '^´=-', 5500000, '^´=-');
+test(decode, ' ', 2, ' ');
+test(decode, ' ', 20, ' ');
+test(decode, ' ', 2000000, ' ');
+test(decode, '123', 2, '123');
+test(decode, '123', 5, '123');
+test(decode, '123', 5500000, '123');
 test(decode, 'zacbde', 5, 'uvxwyz');
 test(decode, 'cnq', 2, 'alo');
 test(decode, 'alo', 26, 'alo'); 
 test(decode, 'Jgnnq Yqtnf!', 2, 'Hello World!'); 
 test(decode, 'fghFGH klmKLM 123', 5, 'abcABC fghFGH 123'); 
-test(decode, '123', 2, '123');
-test(decode, '123', 5, '123');
-test(decode, '123', 5500000, '123');
-test(decode, ' ', 2, ' ');
-test(decode, ' ', 20, ' ');
-test(decode, ' ', 2000000, ' ');
 
 
 
