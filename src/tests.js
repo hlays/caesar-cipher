ed6c3de2d1d9f437d83ea3617487ae25d5589394
hlays <lays.hidani@gmail.com>
2019-02-02 12:29:26 -0200

    add novos testes

--- src/tests.js
+++ src/tests.js
@@ -15,6 +15,8 @@ test(encode, ' ', 2, ' ');
 test(encode, ' ', 20, ' ');
 test(encode, ' ', 2000000, ' ');
 test(encode, '^´=-', 5500000, '^´=-');
+test(encode, '][~,.', 5500000, '][~,.');
+test(encode, '][~,.', -9823, '][~,.');
 // encode: números (esperado retornar o mesmo número)
 test(encode, '123', 2, '123');
 test(encode, '123', 5, '123');
@@ -34,6 +36,8 @@ test(encode, 'abcde', -26, 'abcde');
 console.log('--- Descifrar <<<');
 // decode: espaço e outros caracteres especiais (esperado retornar o mesmo caracter)
 test(decode, '^´=-', 5500000, '^´=-');
+test(decode, '][~,.', 5500000, '][~,.');
+test(decode, '][~,.', -9823, '][~,.');
 test(decode, ' ', 2, ' ');
 test(decode, ' ', 20, ' ');
 test(decode, ' ', 2000000, ' ');
@@ -49,4 +53,4 @@ test(decode, 'Jgnnq Yqtnf!', 2, 'Hello World!');
 test(decode, 'fghFGH klmKLM 123', 5, 'abcABC fghFGH 123'); 
 // decode: offset negativo
 test(decode, 'vwxyz', -5, 'abcde');
-test(encode, 'abcde', -26, 'abcde');
+test(decode, 'abcde', -26, 'abcde');
