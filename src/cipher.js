c9d05f01807c37c44a60c2eef35a63a2992cec7d
hlays <lays.hidani@gmail.com>
2019-05-04 15:55:38 -0300

    improve variables names

--- src/cipher.js
+++ src/cipher.js
@@ -1,69 +1,66 @@
+const alphabetSize = 26;
+const aInAsciiUpperCase = 65;
+const zInAsciiUpperCase = 90;
+const aInAsciiLowerCase = 97;
+const zInAsciiLowerCase = 122;
+
 function cipher() {
-  let eOffset = parseInt(document.getElementById('encode-offset').value);
-  let eText = document.getElementById('encode-text').value;
-  let encodeResult = encode(eOffset, eText);
-  
+  let encondedOffset = parseInt(document.getElementById('encode-offset').value);
+  let encodedText = document.getElementById('encode-text').value;
+  let encodeResult = encode(encondedOffset, encodedText);
+
   document.getElementById('encode-result').value = encodeResult;
 }
 
-
 function decipher() {
-  let dOffset = parseInt(document.getElementById('decode-offset').value);
+  let decodeOffset = parseInt(document.getElementById('decode-offset').value);
   let dText = document.getElementById('decode-text').value;
-  let decodeResult = decode(dOffset, dText);
-  
+  let decodeResult = decode(decodeOffset, dText);
+
   document.getElementById('decode-result').value = decodeResult;
 }
-
 
 function encode(encodeOffset, encodeText) {
   let textEncode = '';
   let letterOffset;
-  
-  for (let i = 0; i < encodeText.length; i++) {    
-    let eCodeAscii = encodeText.charCodeAt(i);
 
-    // Se uppercase
-    if (eCodeAscii >= 65 && eCodeAscii  <= 90) {
+  for (let i = 0; i < encodeText.length; i++) {
+    let encodedCodeAscii = encodeText.charCodeAt(i);
+
+    if (encodedCodeAscii >= aInAsciiUpperCase && encodedCodeAscii <= zInAsciiUpperCase) {
       if (encodeOffset < 0) {
-        letterOffset = (((eCodeAscii - 65) + encodeOffset % 26) + 26) % 26 + 65;
+        letterOffset = (((encodedCodeAscii - aInAsciiUpperCase) + encodeOffset % alphabetSize) + alphabetSize) % alphabetSize + aInAsciiUpperCase;
       } else {
-        letterOffset = ((eCodeAscii - 65 + encodeOffset) % 26) + 65;
+        letterOffset = ((encodedCodeAscii - aInAsciiUpperCase + encodeOffset) % alphabetSize) + aInAsciiUpperCase;
       }
-    // se lowercase
-    } else if (eCodeAscii  >= 97 && eCodeAscii <= 122) {
+    } else if (encodedCodeAscii >= aInAsciiLowerCase && encodedCodeAscii <= zInAsciiLowerCase) {
       if (encodeOffset < 0) {
-        letterOffset = (((eCodeAscii - 97) + encodeOffset % 26) + 26) % 26 + 97;
+        letterOffset = (((encodedCodeAscii - aInAsciiLowerCase) + encodeOffset % alphabetSize) + alphabetSize) % alphabetSize + aInAsciiLowerCase;
       } else {
-        letterOffset = ((eCodeAscii - 97 + encodeOffset) % 26) + 97;
+        letterOffset = ((encodedCodeAscii - aInAsciiLowerCase + encodeOffset) % alphabetSize) + aInAsciiLowerCase;
       }
-    // se outros
     } else {
-      letterOffset = eCodeAscii;
-    } 
+      letterOffset = encodedCodeAscii;
+    }
     textEncode = textEncode + String.fromCharCode(letterOffset);
   }
   return textEncode;
 }
 
-
 function decode(decodeOffset, decodeText) {
   let decodeLetterOffset;
   let textDecode = '';
-  
-  for (let i = 0; i < decodeText.length; i++) {    
+
+  for (let i = 0; i < decodeText.length; i++) {
     let deCodeAscii = decodeText.charCodeAt(i);
 
-    // Se uppercase
-    if (deCodeAscii >= 65 && deCodeAscii  <= 90) {
-      decodeLetterOffset = (((deCodeAscii - 65) - (decodeOffset % 26)) + 26) % 26 + 65;
-    // se lowercase
-    } else if (deCodeAscii  >= 97 && deCodeAscii <= 122) {
-      decodeLetterOffset = (((deCodeAscii - 97) - (decodeOffset % 26)) + 26) % 26 + 97;
-    // se outros
+    if (deCodeAscii >= aInAsciiUpperCase && deCodeAscii <= zInAsciiUpperCase) {
+      decodeLetterOffset = (((deCodeAscii - aInAsciiUpperCase) - (decodeOffset % alphabetSize)) + alphabetSize) % alphabetSize + aInAsciiUpperCase;
+    } else if (deCodeAscii >= aInAsciiLowerCase && deCodeAscii <= zInAsciiLowerCase) {
+      decodeLetterOffset = (((deCodeAscii - aInAsciiLowerCase) - (decodeOffset % alphabetSize)) + alphabetSize) % alphabetSize + aInAsciiLowerCase;
     } else {
       decodeLetterOffset = deCodeAscii;
-    } 
+    }
     textDecode = textDecode + String.fromCharCode(decodeLetterOffset);
   }
   return textDecode;
